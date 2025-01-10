6a29299740f8598779c371e1c1ec82295c1b4ae9
Pashaie <pashaie@gmail.com>
2025-01-10 12:13:41 +0330

    total amount

--- app.js
+++ app.js
@@ -9,10 +9,11 @@
 const formExpense = document.querySelector("#formExpense");
 const descriptionInput = document.querySelector("#description");
 const amountInput = document.querySelector("#amount");
-const totalAmount = document.querySelector("#totalAmount");
+const totalAmountElem = document.querySelector("#totalAmount");
 const expenseTableBody = document.querySelector("#expenseTableBody");
 
-
+const expnese = [];
+//TODO let 
 
 // //event handling
 // formExpense.addEventListener("submit", function (e) {
@@ -36,7 +37,23 @@ formExpense.addEventListener("submit", (e) => {
 
 
     //snippet, clg
-    console.log(description, amount)
+    // console.log(description, amount);
+    //{k1:1, k2:2}
+    // expnese.push({description:description, amount:amount});
+    expnese.push({ description, amount });
 
-
+    descriptionInput.value = "";
+    amountInput.value = "";
+    render();
 });
+
+
+const render = () => {
+    let totalAmount = 0;
+    for (let i = 0; i < expnese.length; i++) {
+        console.log(expnese[i])
+        totalAmount += expnese[i].amount;
+    }
+
+    totalAmountElem.textContent = totalAmount;
+}

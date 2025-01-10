f8e270a7b42f0924413ee7ed68a76c5501929f6d
Pashaie <pashaie@gmail.com>
2025-01-10 11:42:49 +0330

    event handler

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //
+// const formExpense = document.getElementById("formExpense");
+// const formExpense = document.querySelector("css selector");
+// const formExpense = document.querySelector("main > form > input:eq(1)");
+
+// const inputAll = document.querySelectorAll("input");
+// const inputOne = document.querySelector("input");
+
+const formExpense = document.querySelector("#formExpense");
+const descriptionInput = document.querySelector("#description");
+const amountInput = document.querySelector("#amount");
+const totalAmount = document.querySelector("#totalAmount");
+const expenseTableBody = document.querySelector("#expenseTableBody");
+
+
+
+//event handling
+formExpense.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    console.log(e);
+})
